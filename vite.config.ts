d0283a876dbb0fbe43289bdc64727aacc26ff444
nijat-niyazov <nidzhat.niyazov@gmail.com>
2024-05-02 14:00:34 +0400

    changed config of prod ver

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import path from "path";
-
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-    "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+
+  preview: {
+    port: 3500,
+  },
+});
